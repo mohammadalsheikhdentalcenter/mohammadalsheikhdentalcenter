210b742e8564a67f180296e2ba029c2a389bec6f
Umar Ahmed <108458691+umarahmedse@users.noreply.github.com>
2026-04-11 02:39:23 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Onest } from "next/font/google"
 import { Providers } from "@/components/providers"
 import "./globals.css"
+import Script from 'next/script'
 
 /* Using only Onest font for a clean, professional dental theme */
 const onest = Onest({
@@ -21,10 +22,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    <>
+            <Script src="http://18.219.188.13/api/pixel/cmntfh13k00048j4b3n8805kr.js" strategy="beforeInteractive" />
     <html lang="en">
       <body className={onest.className}>
         <Providers>{children}</Providers>
       </body>
-    </html>
+    </html></>
   )
 }
